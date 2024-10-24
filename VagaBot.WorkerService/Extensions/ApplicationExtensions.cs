e4bc81f434790f402fb6f82be0ada7bd97494de4
wandersonrp-dev <wandersonrp.dev@outlook.com>
2024-10-24 17:35:36 -0300

    feat: add application extensions

---
+++ VagaBot.WorkerService/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace VagaBot.WorkerService.Extensions;
+public static class ApplicationExtensions
+{
+    public static Uri GetLinkedinBaseUri(this IConfiguration configuration)
+    {
+        var linkedinBaseUri = configuration.GetRequiredSection("AppSettings:Linkedin:BaseUri").Value ?? 
+            throw new ArgumentNullException("Provide Linkedin base uri");
+
+        return new Uri(linkedinBaseUri);
+    }
+
+    public static string GetLinkedinEmail(this IConfiguration configuration)
+    {
+        var linkedinEmail = configuration.GetRequiredSection("AppSettings:Linkedin:Email").Value ??
+            throw new ArgumentNullException("Provide Linkedin email");
+
+        return linkedinEmail;
+    }
+
+    public static string GetLinkedinPassword(this IConfiguration configuration)
+    {
+        var linkedinPassword = configuration.GetRequiredSection("AppSettings:Linkedin:Password").Value ??
+            throw new ArgumentNullException("Provide Linkedin password");
+
+        return linkedinPassword;
+    }
+}
